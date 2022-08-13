5c5e6e3a4e16f5138561303afb5beb1ed815c613
NikitaS <sergio791309@gmail.com>
2022-08-13 22:22:57 +0300

    Create task 1 seminar 7

---
+++ Task 1/Task 1.cs
@@ -0,0 +1,26 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, columns];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(-999, 1000);
+        Console.Write(array[i, j] + "\t" );
+    }
+    System.Console.WriteLine();
+}
